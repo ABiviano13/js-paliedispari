be532045ab10ea18f101374c1c8caead7d6530b1
ABiviano13 <abiviano0@outlook.com>
2023-02-08 16:09:27 +0100

    COMPLETED Palidroma

--- js/app.js
+++ js/app.js
@@ -2,6 +2,22 @@
 
 
 //Palidroma
-//1. Chiedere all'utente una parola
+//1. Chiedere all'utente una parola e convertirla in minuscolo
+let parolaUtente = prompt('inserisci una parola').toLowerCase();
+console.log(parolaUtente);
+
 //2. Creare una funzione per controllare se la parola inserita è palindroma
+function isPalidroma(parolaPalidroma) {
+    //2.1 Trasformare la parola in un array (.split()), invertirla (reverse()) e concatenare gli elementi (join())
+    const parolaInversa = parolaPalidroma.split('').reverse().join('');
+    return parolaInversa
+}
+
 //3. Comunicare all'utente l'esito del controllo
+let parolaInvertita = isPalidroma(parolaUtente);
+
+if (parolaUtente == parolaInvertita){
+    console.log('La parola è Palidroma');
+} else{
+    console.log('La parola non è Palidroma');
+}
