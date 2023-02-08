ccc43192b665406e928739ed9b3a65ebab60eef4
ABiviano13 <abiviano0@outlook.com>
2023-02-08 15:10:33 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/////FUZIONI/////
+
+
+//Palidroma
+//1. Chiedere all'utente una parola
+//2. Creare una funzione per controllare se la parola inserita è palindroma
+//3. Comunicare all'utente l'esito del controllo
